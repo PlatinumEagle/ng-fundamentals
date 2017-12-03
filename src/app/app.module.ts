126f306cc7d3e7239e8ac3bbaff70d9072b2e084
Rob Mullins <robm@sentinelda.com>
2017-12-03 13:28:44 -0700

    fix flex-layout module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgMaterialModule } from '../ngMaterial/ngMaterial.module';
 
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { EventsListComponent } from './events/events-list.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     NgMaterialModule,
   ],
   providers: [],
